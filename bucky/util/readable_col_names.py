f97ef309f3c0ea5f6c304c638b82e8201b753e0d
Matt Kinsey <matt@mkinsey.com>
2020-08-21 17:18:52 -0400

    add plain english mapping for output column names

---
+++ bucky/util/readable_col_names.py
@@ -0,0 +1,18 @@
+readable_col_names = {
+    "cumulative_cases": "Cumulative Cases",
+    "cumulative_deaths": "Cumulative Deaths",
+    "hospitalizations": "Hospitalizations",
+    "cases_active": "Active Cases",
+    "daily_deaths": "Daily Deaths",
+    "daily_cases": "Daily Cases",
+    "daily_cases_reported": "Daily Reported Cases",
+    "cumulative_cases_reported": "Cumulative Reported Cases",
+    "VENT": "Current Ventilator Usage",
+    "Reff": "Effective Reproductive Number (Reff)",
+    "cases_per_100k" : "Active Cases per 100k",
+    "cases_asymptomatic_active" : "Active Asymptomatic Cases",
+    "CASE_REPORT" : "Case Reporting Rate",
+    "doubling_t" : "Doubling Time",
+    "ICU" : "ICU Beds Usage",
+    "N" : "Population",
+}
