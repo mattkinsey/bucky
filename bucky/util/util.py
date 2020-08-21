a09d9abab8b871793b12396a33a768933d1118ee
Matt Kinsey <matt@mkinsey.com>
2020-08-21 11:30:51 -0400

    should be everything needed to get get_US_data.sh running

---
+++ bucky/util/util.py
@@ -0,0 +1,144 @@
+# TODO break into file now that we have the utils submodule (also update __init__)
+
+import copy
+
+# import lz4.frame
+import lzma  # lzma is slow but reallllly gets that file size down...
+import os
+import pickle
+import threading
+
+import numpy as np
+import pandas as pd
+
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __deepcopy__(self, memo=None):
+        return dotdict({key: copy.deepcopy(value) for key, value in self.items()})
+
+
+def map_np_array(a, d):
+    n = np.ndarray(a.shape)
+    for k in d:
+        n[a == k] = d[k]
+    return n
+
+
+def bin_age_csv(filename, out_filename):
+    df = pd.read_csv(filename, header=None, names=["fips", "age", "N"])
+    df["age_group"] = pd.cut(df["age"], np.append(np.arange(0, 76, 5), 120), right=False)
+    df.groupby(["fips", "age_group"]).sum()["N"].unstack("age_group").to_csv(out_filename)
+
+
+def _cache_files(fname_list, cache_name):
+    tmp = {f: open(f, "rb").read() for f in fname_list}
+    with lzma.open("run_cache/" + cache_name + ".p.xz", "wb") as f:
+        pickle.dump(tmp, f)
+
+
+def cache_files(*argv):
+    thread = threading.Thread(target=_cache_files, args=argv)
+    thread.start()
+
+
+def unpack_cache(cache_file):
+    with lzma.open(cache_file, "rb") as f:
+        tmp = pickle.load(f)
+
+    # os.mkdir(cache_file[:-5])
+    for fname in tmp:
+        new_file = cache_file[:-5] + "/" + fname
+        os.makedirs(os.path.dirname(new_file), exist_ok=True)
+        with open(new_file, "wb") as f:
+            f.write(tmp[fname])
+
+
+def import_numerical_libs(gpu=False):
+    """ Perform imports for libraries with APIs matching numpy, scipy.integrate.ivp, scipy.sparse
+
+    if gpu is True, this these imports will use a monkey-patched version of these modules that has had all it's numpy references replaced with CuPy
+
+    if gpu is False, simply performs:
+        import scipy.integrate._ivp.ivp as ivp
+        import numpy as xp
+        import scipy.sparse as sparse
+
+    returns nothing but imports a version of 'xp', 'ivp', and 'sparse' at the global scope
+    """
+    global xp, ivp, sparse
+    if gpu:
+        import importlib
+        import sys
+
+        # modify src before importing
+        def modify_and_import(module_name, package, modification_func):
+            spec = importlib.util.find_spec(module_name, package)
+            source = spec.loader.get_source(module_name)
+            new_source = modification_func(source)
+            module = importlib.util.module_from_spec(spec)
+            codeobj = compile(new_source, module.__spec__.origin, "exec")
+            exec(codeobj, module.__dict__)
+            sys.modules[module_name] = module
+            return module
+
+        import cupy as cp
+
+        # NB: this isn't correct in general but it works for what scipy solve_ivp needs...
+        def cp_searchsorted(a, v, side="right", sorter=None):
+            if side != "right":
+                raise NotImplementedError
+            if sorter is not None:
+                raise NotImplementedError  # sorter = list(range(len(a)))
+            tmp = v >= a
+            if cp.all(tmp):
+                return len(a)
+            return cp.argmax(~tmp)
+
+        cp.searchsorted = cp_searchsorted
+
+        for name in ("common", "base", "rk", "ivp"):
+            ivp = modify_and_import(
+                "scipy.integrate._ivp." + name,
+                None,
+                lambda src: src.replace("import numpy", "import cupy"),
+            )
+
+        xp = cp
+        import cupyx.scipy.sparse as sparse
+    else:
+        import scipy.integrate._ivp.ivp as ivp
+        import numpy as xp
+        import scipy.sparse as sparse
+
+
+# TODO we should monkeypatch this
+def truncnorm(xp, loc=0.0, scale=1.0, size=1, a_min=None, a_max=None):
+    """ Provides a truncnorm implementation that is compatible with cupy
+    """
+    ret = xp.random.normal(loc, scale, size)
+    if a_min is None:
+        a_min = -xp.inf
+    if a_max is None:
+        a_max = xp.inf
+
+    while True:
+        valid = (ret > a_min) & (ret < a_max)
+        if valid.all():
+            return ret
+        ret[~valid] = xp.random.normal(loc, scale, ret[~valid].shape)
+
+
+def _banner():
+    print(r" ____             _          ")
+    print(r"| __ ) _   _  ___| | ___   _ ")
+    print(r"|  _ \| | | |/ __| |/ / | | |")
+    print(r"| |_) | |_| | (__|   <| |_| |")
+    print(r"|____/ \__,_|\___|_|\_\\__, |")
+    print(r"                       |___/ ")
+    print(r"                             ")
---
+++ get_US_data.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+eval $(parse_yaml config.yml)
+
+mkdir -p $data_dir
+cd $data_dir
+
+# CSSE case data
+if [ ! -d "cases/COVID-19" ]; then
+	mkdir -p cases && pushd cases > /dev/null
+        echo Cloning CSSE repo
+        git -c http.sslVerify=false clone https://github.com/CSSEGISandData/COVID-19.git
+	popd > /dev/null
+fi
+
+# Descartes mobility data
+if [ ! -d "mobility/DL-COVID-19" ]; then
+	mkdir -p mobility && pushd mobility > /dev/null
+        echo Cloning Descartes Labs mobility data
+        git -c http.sslVerify=false clone https://github.com/descarteslabs/DL-COVID-19.git
+	popd > /dev/null
+fi
+
+# COVIDExposureIndices mobility data
+if [ ! -d "mobility/COVIDExposureIndices" ]; then
+        mkdir -p mobility && pushd mobility > /dev/null
+        echo Cloning COVIDExposureIndices mobility data
+        git -c http.sslVerify=false clone https://github.com/COVIDExposureIndices/COVIDExposureIndices.git
+        popd > /dev/null
+fi
+
+# US TL shapefiles
+if [ ! -f "shapefiles/tl_2019_us_county.shp" ]; then
+	mkdir -p shapefiles && pushd shapefiles > /dev/null
+        wget --no-check-certificate https://www2.census.gov/geo/tiger/TIGER2019/COUNTY/tl_2019_us_county.zip
+	unzip -o tl_2019_us_county.zip
+	#rm tl_2019_us_county.zip
+	popd > /dev/null
+fi
+
+if [ ! -f "shapefiles/tl_2019_us_state.shp" ]; then
+        mkdir -p shapefiles && pushd shapefiles > /dev/null
+        wget --no-check-certificate https://www2.census.gov/geo/tiger/TIGER2019/STATE/tl_2019_us_state.zip
+        unzip -o tl_2019_us_state.zip
+        #rm tl_2019_us_state.zip
+        popd > /dev/null
+fi
+
+# US Census bridged-race population estimates (age stratified)
+#https://www.cdc.gov/nchs/nvss/bridged_race/Documentation-Bridged-PostcenV2018.pdf
+if [ ! -f "population/US_pop.csv" ]; then
+        mkdir -p population && pushd population > /dev/null
+        [ ! -f "pcen_v2018_y1018.txt.zip" ] && wget --no-check-certificate https://www.cdc.gov/nchs/nvss/bridged_race/pcen_v2018_y1018.txt.zip
+        [ ! -f "pcen_v2018_y1018.csv" ] && unzip -p pcen_v2018_y1018.txt.zip pcen_v2018_y1018.txt/pcen_v2018_y1018.txt |
+		 cut -c 5-11,86-93 | sed "s/./&,/7;s/./&,/5" > pcen_v2018_y1018.csv
+	[ ! -f "US_pop.csv" ] && PYTHONPATH=../.. python -c "from bucky.util import bin_age_csv; bin_age_csv('pcen_v2018_y1018.csv','US_pop.csv')" 
+        popd > /dev/null
+fi
+
+# Atlantic COVID Tracking Data
+if [ ! -d "cases/covid-tracking-data" ]; then
+        mkdir -p cases && pushd cases > /dev/null
+        echo Cloning Atlantic COVID Tracking Data repo
+        git -c http.sslVerify=false clone https://github.com/COVID19Tracking/covid-tracking-data.git
+        popd > /dev/null
+fi
+
+# Contact matrices
+if [ ! -d "contact_matrices_152_countries" ]; then
+        wget --no-check-certificate https://doi.org/10.1371/journal.pcbi.1005697.s002
+        mv journal.pcbi.1005697.s002 journal.pcbi.1005697.s002.zip
+        unzip journal.pcbi.1005697.s002.zip       
+fi
