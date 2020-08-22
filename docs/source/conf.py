f14f6e6dc2fb184ff334ff79a8d5e850e1abae86
Matt Kinsey <matt@mkinsey.com>
2020-08-22 11:20:29 -0400

    add copybutton to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = ['sphinx.ext.autodoc',
         'sphinxcontrib.tikz',
         'sphinxarg.ext',
         'sphinxcontrib.bibtex',
+        'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
