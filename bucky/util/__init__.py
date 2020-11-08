b76bc221bff0cc982bcd23ad6f89789c3204b890
marounbs <maroun.bousleiman@epfl.ch>
2020-11-08 05:38:57 +0100

    Update __init__.py
    adde remove_chars to init

--- bucky/util/__init__.py
+++ bucky/util/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa  # pylint: disable=missing-module-docstring
-from .util import TqdmLoggingHandler, _banner, cache_files, dotdict, estimate_IFR
+from .util import TqdmLoggingHandler, _banner, cache_files, dotdict, estimate_IFR, remove_chars
