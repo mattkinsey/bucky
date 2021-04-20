8bf2dd1000b5affbda3e65f88b7e19e619bf15bf
Matt Kinsey <matt@mkinsey.com>
2021-04-20 10:48:41 -0400

    add the missing bucky/model/exceptions.py

---
+++ bucky/model/exceptions.py
@@ -0,0 +1,10 @@
+class SimulationException(Exception):
+    """A generic exception to throw when there's an error related to the simulation"""
+
+    pass  # pylint: disable=unnecessary-pass
+
+
+class StateValidationException(SimulationException):
+    """Thrown when the state vector is in an invalid state"""
+
+    pass
