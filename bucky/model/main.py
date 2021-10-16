8d648c4937fef7d6555310043ec08a2ac362e876
Matt Kinsey <matt@mkinsey.com>
2021-10-16 11:13:47 -0400

    fix use of deprecated cupy slicing

--- bucky/model/main.py
+++ bucky/model/main.py
@@ -42,7 +42,7 @@ def get_runid():  # TODO move to util and rename to timeid or something
 
 def frac_last_n_vals(arr, n, axis=0, offset=0):  # TODO assumes come from end of array currently, move to util
     """Return the last n values of an array; if n is a float, including fractional amounts"""
-    int_slice_ind = (
+    int_slice_ind = tuple(
         [slice(None)] * (axis)
         + [slice(-int(n + offset), -int(xp.ceil(offset)) or None)]
         + [slice(None)] * (arr.ndim - axis - 1)
@@ -50,7 +50,7 @@ def frac_last_n_vals(arr, n, axis=0, offset=0):  # TODO assumes come from end of
     ret = arr[int_slice_ind]
     # handle fractional element before the standard slice
     if (n + offset) % 1:
-        frac_slice_ind = (
+        frac_slice_ind = tuple(
             [slice(None)] * (axis)
             + [slice(-int(n + offset + 1), -int(n + offset))]
             + [slice(None)] * (arr.ndim - axis - 1)
@@ -58,7 +58,7 @@ def frac_last_n_vals(arr, n, axis=0, offset=0):  # TODO assumes come from end of
         ret = xp.concatenate((((n + offset) % 1) * arr[frac_slice_ind], ret), axis=axis)
     # handle fractional element after the standard slice
     if offset % 1:
-        frac_slice_ind = (
+        frac_slice_ind = tuple(
             [slice(None)] * (axis)
             + [slice(-int(offset + 1), -int(offset) or None)]
             + [slice(None)] * (arr.ndim - axis - 1)
