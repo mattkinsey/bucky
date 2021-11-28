cf45d201000fffe54f7e2ca458db90faae521845
Matt Kinsey <matt@mkinsey.com>
2021-11-28 10:48:43 -0500

    fix bug in interp_extrap

--- bucky/util/extrapolate.py
+++ bucky/util/extrapolate.py
@@ -11,6 +11,6 @@ def interp_extrap(x, x1, yp, order=2):
         p = xp.poly1d(xp.polyfit(x1[-order - 1 :], yp[-order - 1 :], order))
         y[x > x1[-1]] = p(x[x > x1[-1]])
     if xp.any(x < x1[0]):
-        p = xp.poly1d(xp.polyfit(x1[: order + 1], yp[order + 1], order))
+        p = xp.poly1d(xp.polyfit(x1[: order + 1], yp[: order + 1], order))
         y[x < x1[0]] = p(x[x < x1[0]])
     return y
