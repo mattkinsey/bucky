6471f64afe005b073d7c0e0e85def1cc9b744001
Matt Kinsey <matt@mkinsey.com>
2021-12-14 08:38:59 -0500

    add param for number of points to use for polyfit in interp_extrap

--- bucky/util/extrapolate.py
+++ bucky/util/extrapolate.py
@@ -4,13 +4,15 @@ from ..numerical_libs import sync_numerical_libs, xp
 
 
 @sync_numerical_libs
-def interp_extrap(x, x1, yp, order=2):
+def interp_extrap(x, x1, yp, order=2, n_pts=None):
     """xp.interp function with polynomial extrapolation"""
+    if n_pts is None:
+        n_pts = order + 1
     y = xp.interp(x, x1, yp)
     if xp.any(x > x1[-1]):
-        p = xp.poly1d(xp.polyfit(x1[-order - 1 :], yp[-order - 1 :], order))
+        p = xp.poly1d(xp.polyfit(x1[-n_pts:], yp[-n_pts:], order))
         y[x > x1[-1]] = p(x[x > x1[-1]])
     if xp.any(x < x1[0]):
-        p = xp.poly1d(xp.polyfit(x1[: order + 1], yp[: order + 1], order))
+        p = xp.poly1d(xp.polyfit(x1[:n_pts], yp[:n_pts], order))
         y[x < x1[0]] = p(x[x < x1[0]])
     return y
