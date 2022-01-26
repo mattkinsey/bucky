6741cdb3852e7e9d915362d7f174e2c9a27ac54f
Matt Kinsey <matt@mkinsey.com>
2022-01-26 08:13:07 -0500

    cache output of polyfit b/c it's not deterministic enough and prevents joblib.memory from hitting the cache downstream...

--- bucky/util/extrapolate.py
+++ bucky/util/extrapolate.py
@@ -1,6 +1,18 @@
 """Provides are version of xp.interp that will perform polynomial extrapolation for all OOB x values."""
 
+from joblib import Memory
+
 from ..numerical_libs import sync_numerical_libs, xp
+from ..util.read_config import bucky_cfg
+
+memory = Memory(bucky_cfg["cache_dir"], verbose=0, mmap_mode="r")
+
+
+@memory.cache
+@sync_numerical_libs
+def _cached_polyfit(*args, **kwargs):
+    """Joblib memory cached version of xp.polyfit()"""
+    return xp.polyfit(*args, **kwargs)
 
 
 @sync_numerical_libs
@@ -13,9 +25,9 @@ def interp_extrap(x, x1, yp, order=2, n_pts=None):
             raise ValueError(f"Not enough data points ({len(yp)}) for requested polyfit size ({n_pts})")
     y = xp.interp(x, x1, yp)
     if xp.any(x > x1[-1]):
-        p = xp.poly1d(xp.polyfit(x1[-n_pts:], yp[-n_pts:], order))
+        p = xp.poly1d(_cached_polyfit(x1[-n_pts:], yp[-n_pts:], order))
         y[x > x1[-1]] = p(x[x > x1[-1]])
     if xp.any(x < x1[0]):
-        p = xp.poly1d(xp.polyfit(x1[:n_pts], yp[:n_pts], order))
+        p = xp.poly1d(_cached_polyfit(x1[:n_pts], yp[:n_pts], order))
         y[x < x1[0]] = p(x[x < x1[0]])
     return y
