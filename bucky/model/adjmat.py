2960238181d4b5d815265842c52cd8633344813a
Matt Kinsey <matt@mkinsey.com>
2022-02-23 08:35:33 -0500

    minor fixes to model.adjmat

--- bucky/model/adjmat.py
+++ bucky/model/adjmat.py
@@ -6,19 +6,23 @@ from functools import reduce
 from ..numerical_libs import sync_numerical_libs, xp, xp_sparse
 from ..util.distributions import truncnorm
 
+# TODO Generalize this to include Cij too, mainly perturb/normalize
+
 
 class buckyAij:
     """Class that handles the adjacency matrix for the model, generalizes between dense/sparse."""
 
     @sync_numerical_libs
-    def __init__(self, G, weight_attr="weight", edge_a_min=0.0, force_diag=False, sparse_format="csr", n_nodes=None):
+    def __init__(self, G, weight_attr="weight", force_diag=False, sparse_format="csr", n_nodes=None):
         """Initialize the stored matrix off of the edges of a networkx graph."""
 
         # init the array as sparse so memory doesnt blow up (just in case)
         self.sparse = True
         self.sparse_format = sparse_format
 
-        if force_diag:
+        if ~force_diag:
+            raise NotImplementedError
+        else:
             # cupy is still missing a bunch of dia format functionality :(
             # self.sparse_format = "dia"
             if G is not None:
@@ -26,13 +30,11 @@ class buckyAij:
             else:
                 size = n_nodes
             self._base_Aij = xp_sparse.identity(size, format=sparse_format)
-        else:
-            self._base_Aij = self._read_nx_edge_mat(G, weight_attr=weight_attr, edge_a_min=edge_a_min)
 
         # if sparsity < .5? just automatically make it dense?
         # same if it's fairly small? (<100 rows?)
 
-        self._Aij = self.normalize(self._base_Aij, axis=0)
+        self._Aij = self._normalize(self._base_Aij, axis=0)
         logging.info(f"Loaded Aij: size={self._base_Aij.shape}, sparse={self.sparse}, format={self.sparse_format}")
 
     def todense(self):
@@ -40,15 +42,18 @@ class buckyAij:
         self.sparse = False
         self.sparse_format = None
         self._base_Aij = self._base_Aij.toarray()
+        self._Aij = self._Aij.toarray()
 
     def tosparse(self, sparse_format="csr"):
         """Convert to sparse."""
         self.sparse = True
         self.sparse_format = sparse_format
         self._base_Aij = self._base_Aij.asformat(self.sparse_format)
+        self._Aij = self._Aij.asformat(self.sparse_format)
 
-    def normalize(self, mat, axis=0):
+    def _normalize(self, mat, axis=0):
         """Normalize A along a given axis."""
+
         mat_norm_fac = 1.0 / mat.sum(axis=axis)
         if self.sparse:
             mat = mat.multiply(mat_norm_fac).asformat(self.sparse_format)
@@ -95,15 +100,4 @@ class buckyAij:
         else:
             self._Aij = self._base_Aij * fac
 
-        self._Aij = self.normalize(self._Aij, axis=0)
-
-    def _read_nx_edge_mat(self, G, weight_attr="weight", edge_a_min=0.0):
-        """Read the adj matrix of a networkx graph and convert it to the cupy/scipy sparse format."""
-        # pylint: disable=unused-argument
-        edges = xp.array(list(G.edges(data=weight_attr))).T
-        # TODO fix: edges = edges.T[edges[2] <= a_min].T  # clip edges with weight < a_min
-        # could also do this clipping based of quantiles (i.e. remove 30% of weakest edges)
-        A = xp_sparse.coo_matrix((edges[2], (edges[0].astype(int), edges[1].astype(int))))
-        A = A.asformat(self.sparse_format)
-        A.eliminate_zeros()
-        return A
+        self._Aij = self._normalize(self._Aij, axis=0)
